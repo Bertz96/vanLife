3e0ef2a2544799fdbbef5dbefe2e9e886aec02ba
Bernabe Cerviño <bernycervi96@gmail.com>
2024-04-05 02:20:14 -0300

    cambios en build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build :{
+    outDir:'./dist',
+    },
   plugins: [react()],
   css: {
     postcss: {
