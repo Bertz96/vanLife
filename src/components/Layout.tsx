b4e82f70ccb89c758efcb65a62aa1db97c57e0e3
Bernabe Cerviño <bernycervi96@gmail.com>
2024-05-14 21:05:10 -0300

    multiple bugfixes mobile version

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
+//  En main
+//min-h-screen lg:min-h-min 
+
 export default function Layout() {
   return (
     <div className='lg:flex lg:flex-col min-h-screen mx-auto lg:w-2/3 '>
       <Header />
-      <main className=' min-h-screen lg:mb-8'>
+      <main className=' min-h-screen lg:min-h-min lg:mb-8'>
         <Outlet />
       </main>
       <Footer />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,28 +3,28 @@ import pictureAbout from '../assets/images/about.png'
 
 export default function About() {
   return (
-    <div className=' text-black '>
+    <div className=' min-h-full text-black '>
       <img
         src={pictureAbout}
         alt="foto chico sobre van"
         className=" mx-auto  lg:rounded-md"
       />
 
-      <div className="flex my-5 flex-col gap-3 px-6 lg:px-12">
+      <div className="  flex lg:my-5 flex-col gap-3 px-6 lg:px-12">
         <h1 className=" text-3xl mt-4 lg:mt-0 lg:text-4xl font-bold">
           Don’t squeeze in a sedan when you could relax in a van.
         </h1>
-        <p>
+        <p className='text-lg'>
           Our mission is to enliven your road trip with the perfect travel van
           rental. Our vans are recertified before each trip to ensure your
           travel plans can go off without a hitch. (Hitch costs extra 😉)
         </p>
-        <p>
+        <p className='text-lg'>
           Our team is full of vanlife enthusiasts who know firsthand the magic
           of touring the world on 4 wheels.
         </p>
         {/*  */}
-        <div className=" lg:mb-4  rounded-md bg-[#FFCC8D] lg:p-4 py-4 px-8 lg:px-10">
+        <div className="my-4 lg:my-5 lg:mb-4 rounded-md bg-[#FFCC8D] lg:p-4 py-4 px-8 lg:px-10">
           <h2 className="text-xl lg:text-2xl font-bold">Your destination is waiting.</h2>
           <h2 className="text-xl lg:text-2xl font-bold">Your van is ready.</h2>
           
