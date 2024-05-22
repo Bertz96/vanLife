9b5b30ed37503133899e94ba4d36d6be60794a4d
Bernabe Cerviño <bernycervi96@gmail.com>
2024-05-21 22:50:35 -0300

    mas comentarios y pagina de error

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     //revisar el tema del centrado
-    <div className=" flex flex-col gap-16  px-40 py-64">
+    <div className=" flex flex-col items-center gap-16  lg:px-40 lg:py-64">
       <h1 className=" text-3xl font-bold text-black">
         Sorry, the page you were looking for was not found
       </h1>
 
       <Link
         to="/"
-        className=" rounded-md bg-black py-2 text-center text-xl font-bold  text-white"
+        className=" rounded-md bg-black px-8 py-2 text-center text-xl font-bold  text-white"
       >
         Return to home
       </Link>
